b8cf19a7ffc8cc25816b79daf62a44414cb2dcf8
dev-mint <studiominti@gmail.com>
2020-02-03 12:30:18 -0300

    Create action added

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -8,5 +8,17 @@ class TodosController < ApplicationController
     end
 
     def create
+        @todo = Todo.new(todo_params)
+        if @todo.save
+            flash[:notice] = "Your new ToDo was added successfully"
+            redirect_to root_path
+        else
+            render 'new'
+        end
+    end
+
+    private
+    def todo_params
+        params.require(:todo).permit(:decription, :completed)
     end
 end
