a0c26a89156db15783652b1ddef4cdc9c0fe4646
dev-mint <studiominti@gmail.com>
2020-02-03 13:08:09 -0300

    show action added

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -17,6 +17,10 @@ class TodosController < ApplicationController
         end
     end
 
+    def show
+        @todo = Todo.find(params[:id])
+    end
+
     private
     def todo_params
         params.require(:todo).permit(:decription, :completed)
